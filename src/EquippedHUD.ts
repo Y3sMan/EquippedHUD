9ab96b10e036de96857cf56c21037cf6a59ba7d1
Y3sMan <codmaniac89@gmail.com>
2022-04-07 18:09:11 -0700

    Updated to SP 2.5. Changed main rendering method to SP's 'text' methods. Added a settings file to input x and y positions of the widget

--- src/EquippedHUD.ts
+++ src/EquippedHUD.ts
@@ -1,27 +1,66 @@
-import { browser, once, printConsole, Game, on, Form } from  "skyrimPlatform";
+import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings} from  "skyrimPlatform";
+import { SetIntValue, GetIntValue } from "@skyrim-platform/papyrus-util/StorageUtil"
 
 browser.setVisible(true);
 browser.setFocused(false);
 const visible = browser.isVisible();
 const focused = browser.isFocused();
-browser.loadUrl("file:///Data/Platform/UI/test.html"); 
+
 export function pl() { return Game.getPlayer(); }
+
+var left_text: number
+var right_text: number
+var shout_text: number
+let x = settings["equippedhud"]["x"] as number;
+let y = settings["equippedhud"]["y"] as number;
+var left_offset = -500
+var right_offset = 500
+
+const key = '.equipped-hud.'
+var init = GetIntValue(null, key + 'init', 0)
+// var init = false
+once('update', () => {
+    destroyAllTexts()
+    if ( init == 0 ) {
+        left_text = createText(x + left_offset, y, "Left", [1,1,1,1]);
+        right_text = createText(x + right_offset, y, "Right", [1,1,1,1]);
+        shout_text = createText(x, y, "Shout", [1,1,1,1]);
+        storage[key + 'init.'] = true
+        init = 1
+        SetIntValue(null, key + 'init', 1)
+        SetEquipped(left_text, right_text, shout_text)
+        printConsole('EquippedHUD - SP initialized')
+    }
+});
+on('menuOpen', () => { browser.setVisible(false); });
+on('menuClose', () => { browser.setVisible(true); });
+on('actionBeginDraw', () => { browser.setVisible(false); });
+on('actionBeginSheathe', () => { browser.setVisible(true); });
 on('equip', (e) => {
-    // printConsole('help')
-    let help = 'help'
+    if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
+    SetEquipped(left_text, right_text, shout_text)
+});
+on('unequip', (e) => {
+    if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
+    SetEquipped(left_text, right_text, shout_text)
+});
+
+function SetEquipped(l: number, r: number, s: number) {
     /* 
         0 = left hand
         1 = right hand
         2 = shout slot
     */
-    // if ( e.actor != pl() ) { return; }
     var l_id = pl()?.getEquippedObject(0)
     var r_id = pl()?.getEquippedObject(1)
+    var s_id = pl()?.getEquippedObject(2)
     var l_name = l_id?.getName()
     var r_name = r_id?.getName()
+    var s_name = s_id?.getName()
     if ( l_name == undefined ) { l_name = "None"}
     if ( r_name == undefined ) { r_name = "None"}
-    browser.executeJavaScript(`document.getElementById("left").innerHTML = "${l_name}";`);
-    browser.executeJavaScript(`document.getElementById("right").innerHTML = "${r_name}";`);
-
-});
+    if ( s_name == undefined ) { s_name = "None"}
+    setTextString(l, l_name)
+    setTextString(r, r_name)
+    setTextString(s, s_name)
+}
