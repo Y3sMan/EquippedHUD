7e4b450e3d1b926c5654576736850e427922ac79
Y3sMan <codmaniac89@gmail.com>
2022-04-13 07:57:29 -0700

    v1.1; Actually fixed text widget ids persisting across script reload

--- src/EquippedHUD.ts
+++ src/EquippedHUD.ts
@@ -1,4 +1,4 @@
-import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings} from  "skyrimPlatform";
+import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings, getNumCreatedTexts, getTextString, Utility} from  "skyrimPlatform";
 import { SetIntValue, GetIntValue } from "@skyrim-platform/papyrus-util/StorageUtil"
 
 browser.setVisible(true);
@@ -8,45 +8,65 @@ const focused = browser.isFocused();
 
 export function pl() { return Game.getPlayer(); }
 
-var left_text: number
-var right_text: number
-var shout_text: number
+const key = '.equipped-hud.'
+
+var left_text: number = storage[key+'left.'] as number
+var right_text: number = storage[key+'right.'] as number
+var shout_text: number = storage[key+'shout.'] as number
 let x = settings["equippedhud"]["x"] as number;
 let y = settings["equippedhud"]["y"] as number;
 var left_offset = -500
 var right_offset = 500
 
-const key = '.equipped-hud.'
-var init = GetIntValue(null, key + 'init', 0)
-// var init = false
+var init: number
+storage[key + 'init.'] == false
+once('skyrimLoaded', () => {init = 0});
 once('update', () => {
-    destroyAllTexts()
-    if ( init == 0 ) {
+    // destroyAllTexts()
+    if ( init == 0 && storage[key + 'init.'] == true ) {
         left_text = createText(x + left_offset, y, "Left", [1,1,1,1]);
         right_text = createText(x + right_offset, y, "Right", [1,1,1,1]);
         shout_text = createText(x, y, "Shout", [1,1,1,1]);
+        storage[key+'left.'] = left_text
+        storage[key+'right.'] = right_text
+        storage[key+'shout.'] = shout_text
         storage[key + 'init.'] = true
         init = 1
         SetIntValue(null, key + 'init', 1)
         SetEquipped(left_text, right_text, shout_text)
         printConsole('EquippedHUD - SP initialized')
     }
+    // printConsole(`The number of texts is ${getNumCreatedTexts()}`)
+    // for (let i = getNumCreatedTexts(); i > 0; i--) {
+    //     printConsole(`The widget id is ${i} and it's string is ${getTextString(i)}`)
+    // }
 });
 on('menuOpen', () => { browser.setVisible(false); });
 on('menuClose', () => { browser.setVisible(true); });
+var flag = false
 on('actionBeginDraw', () => { 
-    printConsole('begin draw')
-    browser.setVisible(true); });
+    flag = true
+    browser.setVisible(true); 
+});
 on('actionBeginSheathe', () => { 
-    printConsole('begin sheathe')
-    browser.setVisible(false); });
+    flag = false
+    fadeout()
+});
+const fadeout = async () => {
+    await Utility.wait(3.0);
+    if ( !flag ){browser.setVisible(false); }
+}
 on('equip', (e) => {
+    browser.setVisible(true); 
     if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
     SetEquipped(left_text, right_text, shout_text)
+    fadeout()
 });
 on('unequip', (e) => {
+    browser.setVisible(true); 
     if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
     SetEquipped(left_text, right_text, shout_text)
+    fadeout()
 });
 
 function SetEquipped(l: number, r: number, s: number) {
