c5192c9d85ee143545f3fa3aa8778cfa5b5bb04b
Y3sMan <codmaniac89@gmail.com>
2022-04-16 22:40:10 -0700

    Improved compatibility with other SP text plugins. Improved initialization of plugin.

--- src/EquippedHUD.ts
+++ src/EquippedHUD.ts
@@ -1,14 +1,11 @@
-import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings, getNumCreatedTexts, getTextString, Utility} from  "skyrimPlatform";
-import { SetIntValue, GetIntValue } from "@skyrim-platform/papyrus-util/StorageUtil"
-
+import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings, Actor, getTextString, Utility, setTextColor, getTextColor, getNumCreatedTexts} from  "skyrimPlatform";
+import { GetIntValue, SetIntValue } from "@skyrim-platform/papyrus-util/StorageUtil"
 browser.setVisible(true);
 browser.setFocused(false);
-const visible = browser.isVisible();
-const focused = browser.isFocused();
 
-export function pl() { return Game.getPlayer(); }
+function pl() { return Game.getPlayer(); }
 
-const key = '.equipped-hud.'
+const key = '.equipped-hud'
 
 var left_text: number = storage[key+'left.'] as number
 var right_text: number = storage[key+'right.'] as number
@@ -18,58 +15,86 @@ let y = settings["equippedhud"]["y"] as number;
 var left_offset = -500
 var right_offset = 500
 
-var init: number
-storage[key + 'init.'] == false
-once('skyrimLoaded', () => {init = 0});
+var plugin_init = key + '.init.'
+var init = storage[plugin_init] as number | 0
+storage[plugin_init] == false
+// let allowInit = storage[plugin_init] as boolean | false;
+const WasInitialized = () => GetIntValue(null, plugin_init, 0);
+const MarkInitialized = () => SetIntValue(null, plugin_init, 1);
+
+const InitPlugin = function(){
+    left_text = createText(x + left_offset, y, "Left", [1,1,1,1]);
+    right_text = createText(x + right_offset, y, "Right", [1,1,1,1]);
+    shout_text = createText(x, y, "Shout", [1,1,1,1]);
+    storage[key+'left.'] = left_text
+    storage[key+'right.'] = right_text
+    storage[key+'shout.'] = shout_text
+    storage[plugin_init] = true
+    MarkInitialized()
+    init = 1
+    printConsole('EquippedHUD - SP initialized')
+}
+
 once('update', () => {
-    // destroyAllTexts()
-    if ( init == 0 && storage[key + 'init.'] == true ) {
-        left_text = createText(x + left_offset, y, "Left", [1,1,1,1]);
-        right_text = createText(x + right_offset, y, "Right", [1,1,1,1]);
-        shout_text = createText(x, y, "Shout", [1,1,1,1]);
-        storage[key+'left.'] = left_text
-        storage[key+'right.'] = right_text
-        storage[key+'shout.'] = shout_text
-        storage[key + 'init.'] = true
-        init = 1
-        SetIntValue(null, key + 'init', 1)
-        SetEquipped(left_text, right_text, shout_text)
-        printConsole('EquippedHUD - SP initialized')
+    if ( init == 0 || WasInitialized() == 0) {
+        InitPlugin()
     }
-    // printConsole(`The number of texts is ${getNumCreatedTexts()}`)
-    // for (let i = getNumCreatedTexts(); i > 0; i--) {
-    //     printConsole(`The widget id is ${i} and it's string is ${getTextString(i)}`)
-    // }
 });
-on('menuOpen', () => { browser.setVisible(false); });
-on('menuClose', () => { browser.setVisible(true); });
-var flag = false
-on('actionBeginDraw', () => { 
-    flag = true
-    browser.setVisible(true); 
+on('loadGame', () => {
+    init = 0
+    InitPlugin()
+    SetEquipped()
 });
-on('actionBeginSheathe', () => { 
-    flag = false
+var bfadein = false
+var bfadeout = false
+on('menuOpen', () => { browser.setVisible(false) });
+on('menuClose', () => {  browser.setVisible(true);fadein() });
+on('actionEndDraw', () => { 
+    fadein()
+});
+on('actionEndSheathe', () => { 
     fadeout()
 });
 const fadeout = async () => {
+    bfadeout= true
     await Utility.wait(3.0);
-    if ( !flag ){browser.setVisible(false); }
+    if ( bfadeout ){
+        var widgets: number[] = [ storage[key+'left.'] as number, storage[key+'right.'] as number, storage[key+'shout.'] as number]
+        // var widgets: number[]  = [ left_text, right_text, shout_text]
+        widgets.forEach(w => {
+            let c = getTextColor(w)
+            c[3] = 0
+            setTextColor(w, c) 
+        }); 
+    }
+    bfadeout = false
+}
+function fadein() {
+    bfadein = true
+    bfadeout = false
+    // if ( !bfadeout ){
+    let widgets: number[] = [ storage[key+'left.'] as number, storage[key+'right.'] as number, storage[key+'shout.'] as number]
+    // var widgets: number[]  = [ left_text, right_text, shout_text]
+    widgets.forEach(w => {
+        let c = getTextColor(w)
+        c[3] = 1
+        setTextColor(w, c) 
+    }); 
+    // }
+    bfadein = false
 }
 on('equip', (e) => {
-    browser.setVisible(true); 
-    if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
-    SetEquipped(left_text, right_text, shout_text)
-    fadeout()
+    fadein()
+    SetEquipped()
+    if (!pl()?.isWeaponDrawn()) {fadeout()}
 });
 on('unequip', (e) => {
-    browser.setVisible(true); 
-    if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
-    SetEquipped(left_text, right_text, shout_text)
-    fadeout()
+    fadein()
+    SetEquipped()
+    if (!pl()?.isWeaponDrawn()) {fadeout()}
 });
 
-function SetEquipped(l: number, r: number, s: number) {
+function SetEquipped() {
     /* 
         0 = left hand
         1 = right hand
@@ -84,7 +109,7 @@ function SetEquipped(l: number, r: number, s: number) {
     if ( l_name == undefined ) { l_name = "None"}
     if ( r_name == undefined ) { r_name = "None"}
     if ( s_name == undefined ) { s_name = "None"}
-    setTextString(l, l_name)
-    setTextString(r, r_name)
-    setTextString(s, s_name)
+    setTextString(left_text, l_name)
+    setTextString(right_text, r_name)
+    setTextString(shout_text, s_name)
 }
