786101374aaf2d9a0f4ebc069e1ef35fd7fe5618
YesMan <codmaniac89@gmail.com>
2022-07-20 13:18:35 -0700

    switched to spTextUtil instead of my own WidgetTools

--- src/EquippedHUD.ts
+++ src/EquippedHUD.ts
@@ -1,6 +1,7 @@
 import { browser, once, printConsole, Game, on, createText, setTextString, destroyAllTexts, storage, settings, getNumCreatedTexts, getTextString, Utility} from  "skyrimPlatform";
 import { SetIntValue, GetIntValue } from "@skyrim-platform/papyrus-util/StorageUtil"
-import * as wt from "@skyrim-platform/WidgetTools/WidgetTools";
+// import * as wt from "@skyrim-platform/WidgetTools/WidgetTools";
+import * as wt from '../../modules/SPTextUtils/spTextUtils'
 
 browser.setVisible(true);
 browser.setFocused(false);
@@ -11,11 +12,13 @@ export function pl() { return Game.getPlayer(); }
 
 const key = '.equipped-hud.'
 
-var left_hand: number = 0
-var right_hand: number = 0
-var shoutslot: number = 0
-let x = settings["equippedhud"]["x"] as number;
-let y = settings["equippedhud"]["y"] as number;
+var left_hand: wt.spText 
+var right_hand: wt.spText 
+var shoutslot: wt.spText
+// let x = settings["equippedhud"]["x"] as number;
+// let y = settings["equippedhud"]["y"] as number;
+let x: number = 1500
+let y: number = 1000
 var left_offset = -500
 var right_offset = 500
 
@@ -24,25 +27,25 @@ var modname: string = 'EquippedHUD'
 const colors = {
     white: [1, 1, 1, 1]
 }
+wt.spText.destroyAllModTexts(modname)
+left_hand = new wt.spText(x +left_offset, y, 'left', colors['white'], undefined, modname)
+right_hand = new wt.spText(x +right_offset, y, 'right', colors['white'], undefined, modname)
+shoutslot = new wt.spText(x, y, 'shout', colors['white'], undefined, modname)
 
-storage[key + 'init.'] == false
+// storage[key + 'init.'] == false
 once('skyrimLoaded', () => {init = 0});
 once('update', () => {
-    wt.DestroyAllModWidgets(modname)
-    left_hand = wt.CreateText('left', 'left_hand', modname, x + left_offset, y, colors['white'])
-    right_hand = wt.CreateText('right', 'right_hand', modname, x + right_offset, y, colors['white'])
-    shoutslot = wt.CreateText('shout', 'shoutslot', modname, x, y, colors['white'])
     init = 1
     SetIntValue(null, key + 'init', 1)
     SetEquipped(left_hand, right_hand, shoutslot)
     printConsole('EquippedHUD - SP initialized')
 });
-on('menuOpen', () => {wt.FadeAllModTexts(modname, false)});
-on('menuClose', () => {wt.FadeAllModTexts(modname, true)});
+on('menuOpen', () => {wt.spText.disableModTexts(modname)});
+on('menuClose', () => {wt.spText.enableModTexts(modname)});
 var flag = false
 on('actionBeginDraw', () => { 
     flag = true
-    wt.FadeAllModTexts(modname, true)
+    wt.spText.enableModTexts(modname)
 });
 on('actionBeginSheathe', () => { 
     flag = false
@@ -50,7 +53,7 @@ on('actionBeginSheathe', () => {
 });
 const fadeout = async () => {
     await Utility.wait(3.0);
-    if ( !flag ){wt.FadeAllModTexts(modname, false)}
+    if ( !flag ){wt.spText.disableModTexts(modname)}
 }
 on('equip', (e) => {
     if ( e.actor.getFormID() != pl()?.getFormID() ) { return; }
@@ -61,13 +64,13 @@ on('unequip', (e) => {
     SetEquipped(left_hand, right_hand, shoutslot)
 });
 
-function SetEquipped(l: number, r: number, s: number) {
+function SetEquipped(l: wt.spText, r: wt.spText, s: wt.spText) {
     /* 
         0 = left hand
         1 = right hand
         2 = shoutslot slot
     */
-    wt.FadeAllModTexts(modname, true)
+   wt.spText.enableModTexts(modname)
     var l_id = pl()?.getEquippedObject(0)
     var r_id = pl()?.getEquippedObject(1)
     var s_id = pl()?.getEquippedObject(2)
@@ -77,8 +80,9 @@ function SetEquipped(l: number, r: number, s: number) {
     if ( l_name == undefined ) { l_name = "None"}
     if ( r_name == undefined ) { r_name = "None"}
     if ( s_name == undefined ) { s_name = "None"}
-    wt.EditModTextString(l_name, modname, 'left_hand')
-    wt.EditModTextString(r_name, modname, 'right_hand')
-    wt.EditModTextString(s_name, modname, 'shoutslot')
+
+    left_hand.setText(l_name)
+    right_hand.setText(r_name)
+    shoutslot.setText(s_name)
     fadeout()
 }
